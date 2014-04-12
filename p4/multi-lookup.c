bb4254495c2b2d00af24b93be6940b6212195cb2
Michael <michael@michael-VirtualBox.(none)>
2014-04-11 18:23:23 -0700

    Getting closer

--- p4/multi-lookup.c
+++ p4/multi-lookup.c
@@ -6,71 +6,104 @@
 
 #include "multi-lookup.h"
 
-void* PushFileNames(void* filename)
-{
-	fprintf(stdout, " Thread holds filename: %s\n", (char*)filename);
-	while(queue_is_full(&q))
-		usleep((rand()%100)*10000+1000000);	
-	
-	queue_push(&q, filename);
-
-	/* Exiting */
-	return NULL;
-}
-
 int main(int argc, char* argv[])
 {
-	/* void unused vars */
 	(void)argc;
 	(void)argv;
-
-	int i;
-	/* Thread variables */
-	
-	int rc;
-
-	/* Check Arguments */
 	if(argc < MINARGS)
-    {
+	{
         fprintf(stderr, "Not enough arguments: %d\n", (argc-1));
         fprintf(stderr, "Usage:\n %s %s\n", argv[0], USAGE);
         return EXIT_FAILURE;
-    }
+	}
+	outputfp = fopen(argv[argc-1], "w");
+	
+	if(!outputfp)
+	{
+		perror("Error opening output file.");
+		return EXIT_FAILURE;
+	}
 
-	/* Initialize queue */
-	i = queue_init(&q, MAX_INPUT_FILES);
-
-
+	if(queue_init(&q, QUEUE_LENGTH) == QUEUE_FAILURE)
+		fprintf(stderr, "Queue initialization failed.\n");
+	
+	pthread_mutex_init(&queue_lock, NULL);	
+	
 	for(i = 0; i<argc-2; i++)
 	{
-		rc = pthread_create(&(req_threads[i]), NULL, PushFileNames, argv[i+1]);
+		rc = pthread_create(&(req_threads[i]), NULL, Requester, argv[i+1]);
 		if(rc)
-			fprintf(stderr, "Error creating thread %d.\n", i);
-		else
-			fprintf(stdout, "Successfully created thread %d.\n", i);
+		{
+			fprintf(stderr, "Error creating requester thread %d.\n", i);
+			return EXIT_FAILURE;
+		}
 	}
+	
+	pthread_mutex_init(&output_lock, NULL);
+	
+	for(i = 0; i < THREAD_MAX; i++)
+	{
+		res_id[i] = i;
+		rc = pthread_create(&(res_threads[i]), NULL, Resolver, &res_id[i]);
+		if(rc)
+		{
+			fprintf(stderr, "Error creating resolver thread %d.\n", i);
+			return EXIT_FAILURE;
+		}
+	}
+
 	for(i = 0; i<argc-2; i++)
 		pthread_join(req_threads[i], NULL);
+	
+	for(i = 0; i<THREAD_MAX; i++)
+		pthread_join(res_threads[i], NULL);
+
     return EXIT_SUCCESS;
 }
 
-
-/* THIS HAPPENS LATER
-FILE* inputfp = fopen(*filename, "r");
-if(!inputfp)
+void* Requester(void* filename)
 {
-	sprintf(errorstr, "\nError opening input file: \"%s\"", argv[i]);
-	perror(errorstr);
-	continue;
-}*/
+	FILE* inputfp = fopen((char*)filename, "r");
+	char hostname[MAX_NAME_LENGTH];
+	if(!inputfp)
+	{
+		fprintf(stderr, "Error opening input file: \"%s\"\n", (char*)filename);
+		return NULL;
+	}
+	while(fscanf(inputfp, INPUTFS, hostname) > 0)
+	{
+		if(!queue_is_full(&q))
+		{
+			//pthread_mutex_lock(&queue_lock);
+			queue_push(&q, &hostname);
+			fprintf(stdout, "Pushed address \"%s\" to the queue.\n", hostname);
+			//pthread_mutex_unlock(&queue_lock);
+		}
+		else
+			usleep((rand()%100)*10000+1000000);
+	}
+	return NULL;
+}
 
+void* Resolver(void* arg)
+{
+	(void)arg;
+	usleep(30000);
+	fprintf(stdout, "Resolver thread %d created.\n", *(int*)arg);
+	while(!queue_is_empty(&q))
+	{
+		//pthread_mutex_lock(&queue_lock);
+		char* hostname = queue_pop(&q);
+		fprintf(stdout, "Resolver thread %d popped address %s\n", *(int*)arg, hostname);
+		//free(hostname);
+		//pthread_mutex_unlock(&queue_lock);
+	}
+	fprintf(stdout, "Empty queue, thread %d quitting.", *(int*)arg);
+	return NULL;
+}
 /* Setup Output File
 outputfp = fopen(argv[argc-1], "w");
-if(!outputfp)
-{
-	perror("Error opening output file.");
-	return EXIT_FAILURE;
-}
+
 
 
 
--- p4/multi-lookup.h
+++ p4/multi-lookup.h
@@ -8,13 +8,27 @@
 #include "util.h"
 #include "queue.h"
 
+#define USAGE "<inputFilePath> <outputFilePath>"
+#define MIN_RESOLVER_THREADS 2
+#define MAX_NAME_LENGTH 1025
 #define MAX_INPUT_FILES 10
+#define INPUTFS "%1024s"
+#define QUEUE_LENGTH 50
 #define THREAD_MAX 5
 #define MINARGS 3
-#define USAGE "<inputFilePath> <outputFilePath>"
-#define MAX_NAME_LENGTH 1025
-#define INPUTFS "%1024s"
+
+pthread_t req_threads[MAX_INPUT_FILES];
+pthread_t res_threads[THREAD_MAX];
+
+pthread_mutex_t queue_lock;
+pthread_mutex_t output_lock;
+
+FILE* outputfp;
 
 queue q;
-int test = 10;
-pthread_t req_threads[THREAD_MAX];
+int res_id[THREAD_MAX];
+int rc;
+int i;
+
+void* Requester(void*);
+void* Resolver(void*);
